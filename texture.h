a73c74cabe1c0b68749335c96c50dc1e6dab2683
zacarywebb <133690854+zacarywebb@users.noreply.github.com>
2024-07-29 12:56:47 -0400

    Add files via upload

---
+++ texture.h
@@ -0,0 +1,27 @@
+#ifndef MINESWEEPER_TEXTURE_H
+#define MINESWEEPER_TEXTURE_H
+#include <iostream>
+#include <map>
+#include <string>
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+class Texture{
+private:
+    map<string, sf::Texture> textures;
+
+public:
+    void loadTexture(const string& filename) {
+        sf::Texture texture;
+        if (texture.loadFromFile(filename)) {
+            textures[filename] = texture;
+        }
+    }
+
+    sf::Texture& getTexture(const string& filename) {
+        return textures.at(filename);
+    }
+
+};
+
+#endif //MINESWEEPER_TEXTURE_H
